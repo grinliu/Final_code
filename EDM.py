e8e93bbd4d68ec3558d6de0a659641815551641f
abxm2 <wulimaxh@gmail.com>
2023-06-21 10:05:25 +0800

    update codes

--- EDM.py
+++ EDM.py
@@ -1,13 +1,12 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate, StratifiedKFold
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, classification_report
 
 # Read the data from the Excel file
-file_name = "/Users/liujiahua/Library/CloudStorage/OneDrive-Personal/文档/edm_cluster1.xlsx"
+file_name = "edm_cluster1.xlsx"
 sheet_name = "Sheet1"
 df = pd.read_excel(file_name, sheet_name=sheet_name)
 
@@ -21,34 +20,41 @@ for col in df.columns:
 X = df.drop(['Class'], axis=1).values
 y = df['Class'].values
 
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
 # Scale input features
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_scaled = scaler.fit_transform(X)
 
-# Create and train classifiers
+# Create classifiers
 ann_classifier = MLPClassifier(hidden_layer_sizes=(100,100), max_iter=1000)
-ann_classifier.fit(X_train_scaled, y_train)
-
 dt_classifier = DecisionTreeClassifier()
-dt_classifier.fit(X_train, y_train)
-
 nb_classifier = GaussianNB()
-nb_classifier.fit(X_train, y_train)
 
-# Predictions
-y_pred_ann = ann_classifier.predict(X_test_scaled)
-y_pred_dt = dt_classifier.predict(X_test)
-y_pred_nb = nb_classifier.predict(X_test)
+# Initialize 10-fold cross-validation
+cv = StratifiedKFold(n_splits=10)
 
-# Classification Reports
-ann_report = classification_report(y_test, y_pred_ann)
-dt_report = classification_report(y_test, y_pred_dt)
-nb_report = classification_report(y_test, y_pred_nb)
+# Cross-validate metrics
+def cross_val_metrics(model, X, y, cv, scoring_metrics, n_jobs=-1):
+    scores = cross_validate(model, X, y, cv=cv, scoring=scoring_metrics, n_jobs=n_jobs)
+    return {metric: scores[f"test_{metric}"].mean() for metric in scoring_metrics}
 
-print("ANN Classification Report:\n", ann_report)
-print("DT Classification Report:\n", dt_report)
-print("NB Classification Report:\n", nb_report)
+scoring_metrics = ['accuracy', 'precision_macro', 'recall_macro', 'f1_macro']
+
+# Calculate cross-validated metrics for each classifier
+ann_eval_metrics = cross_val_metrics(ann_classifier, X_scaled, y, cv, scoring_metrics)
+dt_eval_metrics = cross_val_metrics(dt_classifier, X, y, cv, scoring_metrics)
+nb_eval_metrics = cross_val_metrics(nb_classifier, X, y, cv, scoring_metrics)
+
+print("ANN evaluation metrics with 10-Fold CV:")
+for metric, mean_value in ann_eval_metrics.items():
+    print(f"{metric}: {mean_value}")
+print()
+
+print("DT evaluation metrics with 10-Fold CV:")
+for metric, mean_value in dt_eval_metrics.items():
+    print(f"{metric}: {mean_value}")
+print()
+
+print("NB evaluation metrics with 10-Fold CV:")
+for metric, mean_value in nb_eval_metrics.items():
+    print(f"{metric}: {mean_value}")
+print()
